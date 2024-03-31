c0e79d9e24003c8d3b23fe546a7796b85ff19665
Aleksei Zhemerikin <zhemerikin.samspace@gmail.com>
2024-03-31 09:18:26 +0400

    Add form in feedback

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
 import classes from './Button.module.css'
 
-function Button( {children, onClick, isActive} ) {
+function Button( {children, isActive, ...props} ) {
 
   //console.log(classes)
 
   return (
-    <button className={isActive ? `${classes.button} ${classes.active}` : classes.button} onClick={onClick}>
+    <button 
+      className={isActive ? `${classes.button} ${classes.active}` : classes.button} 
+      {...props}
+    >
         {children}
     </button>
   )
--- src/components/FeedBackSection.jsx
+++ src/components/FeedBackSection.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react"
+import Button from "./Button/Button"
 
 export default function FeedBackSection() {
+
+  const [name, setName] = useState('')
+  const [hasError, setHasError] = useState(false)
+  const [reason, setReason] = useState('suggest')
+
+  function handleNameChange(event){
+    setName(event.target.value)
+    setHasError(event.target.value.trim().length == 0)
+  }
+
   return (
     <section>
         <h3>FeedBack</h3>
+
+        <form>
+          <label htmlFor="name">Your name</label>
+          <input 
+            type="text" 
+            id="name" 
+            className="control" 
+            value={name}
+            style = {{
+              border: hasError ? '1px solid red' : null,
+            }}
+            onChange={handleNameChange}            
+          />
+
+          <label htmlFor="reason">Reason</label>
+          <select id="reason" className="control" value={reason} onChange={event => setReason(event.target.value)}>
+            <option value="error">Error</option>
+            <option value="help">Help</option>
+            <option value="suggest">Suggest</option>
+          </select>
+
+          <pre>
+            <hr />
+            Name: {name}
+            <br />
+            Reason: {reason}
+            <br />
+            hasError: {hasError}
+            <hr />
+          </pre>
+
+          <Button disabled = {hasError} isActive={!hasError}>Send</Button>
+        </form>        
     </section>
   )
 }
